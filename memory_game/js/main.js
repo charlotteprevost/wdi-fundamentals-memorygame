524bfc28b1b735d22ceb692f47eeeec6a7d796eb
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-01 15:18:40 -0500

    created gane logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,24 +20,37 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
-var cardsInPlay =[];
+
+var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
-		else {
-			alert("Sorry, try again.");
-		}
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped a " + cards[cardId].rank + " of " + cards[cardId].suit + ".");
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
